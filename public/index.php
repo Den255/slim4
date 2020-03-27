3afea8bd4febca88116124e80efcff9b53828eee
Denis Abramov <den255work@gmail.com>
2020-03-27 15:32:48 +0200

    -

--- public/index.php
+++ public/index.php
@@ -3,10 +3,10 @@ namespace App;
 
 use Slim\Middleware\ErrorMiddleware;
 use Slim\Factory\AppFactory;
-
+use \DI\Container;
 require __DIR__ . '/../vendor/autoload.php';
 
-$container = new \DI\Container();
+$container = new Container();
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 
